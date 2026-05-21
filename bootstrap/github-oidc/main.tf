55c1b5f67a45d0ccf361095ee34d7e054f5098d9
Christopher Collin Løkken <christopherl@mnemonic.no>
2026-05-21 14:34:26 +0200

    fix: main tf

--- bootstrap/github-oidc/main.tf
+++ bootstrap/github-oidc/main.tf
@@ -1,47 +1,24 @@
 data "azurerm_subscription" "current" {}
 
-resource "random_string" "state_suffix" {
-  count = var.state_storage_account_name == null ? 1 : 0
-
-  length  = 8
-  upper   = false
-  special = false
-}
-
-locals {
-  state_storage_account_name = coalesce(var.state_storage_account_name, "sttfstate${random_string.state_suffix[0].result}")
+resource "azurerm_resource_group" "identity" {
+  name     = var.identity_resource_group_name
+  location = var.location
 }
 
 resource "azurerm_resource_group" "state" {
   name     = var.state_resource_group_name
   location = var.location
-  tags     = var.tags
 }
 
 resource "azurerm_storage_account" "state" {
-  name                     = local.state_storage_account_name
+  name                     = var.state_storage_account_name
   resource_group_name      = azurerm_resource_group.state.name
   location                 = azurerm_resource_group.state.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
 
+  min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = false
-
-  blob_properties {
-    versioning_enabled = true
-
-    delete_retention_policy {
-      days = 14
-    }
-
-    container_delete_retention_policy {
-      days = 14
-    }
-  }
-
-  tags = var.tags
 }
 
 resource "azurerm_storage_container" "state" {
@@ -50,29 +27,22 @@ resource "azurerm_storage_container" "state" {
   container_access_type = "private"
 }
 
-resource "azurerm_resource_group" "identity" {
-  name     = var.identity_resource_group_name
-  location = var.location
-  tags     = var.tags
-}
-
 resource "azurerm_user_assigned_identity" "github_actions" {
   name                = var.identity_name
   location            = azurerm_resource_group.identity.location
   resource_group_name = azurerm_resource_group.identity.name
-  tags                = var.tags
 }
 
 resource "azurerm_federated_identity_credential" "github_environment" {
-  for_each = var.github_environments
+  for_each = toset(var.github_environments)
 
-  name                = "github-${var.github_repository}-${each.value}"
+  name                = "github-${var.github_repository}-${each.key}"
   resource_group_name = azurerm_resource_group.identity.name
   parent_id           = azurerm_user_assigned_identity.github_actions.id
 
-  issuer   = "https://token.actions.githubusercontent.com"
   audience = ["api://AzureADTokenExchange"]
-  subject  = "repo:${var.github_organization}/${var.github_repository}:environment:${each.value}"
+  issuer   = "https://token.actions.githubusercontent.com"
+  subject  = "repo:${var.github_organization}/${var.github_repository}:environment:${each.key}"
 }
 
 resource "azurerm_role_assignment" "terraform_subscription" {
@@ -81,8 +51,8 @@ resource "azurerm_role_assignment" "terraform_subscription" {
   principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
 }
 
-resource "azurerm_role_assignment" "terraform_state_blob_data_contributor" {
+resource "azurerm_role_assignment" "terraform_state_blob_data_owner" {
   scope                = azurerm_storage_account.state.id
-  role_definition_name = "Storage Blob Data Contributor"
+  role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
 }
