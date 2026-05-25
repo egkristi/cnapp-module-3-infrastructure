16083bbda31fd2da350bc7d45313a94d5c4e223c
markusstroemme <72066292+markusstroemme@users.noreply.github.com>
2026-05-25 12:35:17 +0000

    Wait for parent + 90s

--- modules/app-gw-for-containers/main.tf
+++ modules/app-gw-for-containers/main.tf
@@ -9,10 +9,27 @@ resource "azurerm_application_load_balancer" "this" {
 resource "azurerm_application_load_balancer_frontend" "this" {
   name                         = var.frontend_name
   application_load_balancer_id = azurerm_application_load_balancer.this.id
+
+  depends_on = [
+    time_sleep.wait_for_agfc
+  ]
 }
+
 
 resource "azurerm_application_load_balancer_subnet_association" "this" {
   name                         = var.association_name
   application_load_balancer_id = azurerm_application_load_balancer.this.id
   subnet_id                    = var.association_subnet_id
+
+  depends_on = [
+    time_sleep.wait_for_agfc
+  ]
+}
+
+resource "time_sleep" "wait_for_agfc" {
+  depends_on = [
+    azurerm_application_load_balancer.this
+  ]
+
+  create_duration = "90s"
 }
