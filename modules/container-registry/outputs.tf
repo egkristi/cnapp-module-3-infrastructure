058fe93fda852f399b89130c9c7340cf1d21ac50
markusstroemme <72066292+markusstroemme@users.noreply.github.com>
2026-05-28 08:58:50 +0000

    La til outputs i feil mappe 👉👈

--- modules/container-registry/outputs.tf
+++ modules/container-registry/outputs.tf
@@ -1,34 +1,14 @@
-output "shared_services_resource_group_name" {
-  description = "Name of the shared services resource group."
-  value       = module.shared_services_resource_group.name
+output "name" {
+  description = "Name of the Azure Container Registry."
+  value       = azurerm_container_registry.this.name
 }
 
-output "new_acr_name" {
-  description = "Name of the new Azure Container Registry."
-  value       = module.container_registry.name
+output "id" {
+  description = "ID of the Azure Container Registry."
+  value       = azurerm_container_registry.this.id
 }
 
-output "new_acr_id" {
-  description = "ID of the new Azure Container Registry."
-  value       = module.container_registry.id
-}
-
-output "new_acr_login_server" {
-  description = "Login server URL of the new Azure Container Registry."
-  value       = module.container_registry.login_server
-}
-
-output "existing_acr_name" {
-  description = "Name of the existing Azure Container Registry."
-  value       = data.azurerm_container_registry.existing.name
-}
-
-output "existing_acr_id" {
-  description = "ID of the existing Azure Container Registry."
-  value       = data.azurerm_container_registry.existing.id
-}
-
-output "existing_acr_login_server" {
-  description = "Login server URL of the existing Azure Container Registry."
-  value       = data.azurerm_container_registry.existing.login_server
+output "login_server" {
+  description = "Login server URL of the Azure Container Registry."
+  value       = azurerm_container_registry.this.login_server
 }
