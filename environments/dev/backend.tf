7076904328e46ceb95aeb7b1407ad85643de075c
Kristiansen, Erling Gustav Moland <Erling.Kristiansen@skatteetaten.no>
2026-06-02 09:33:36 +0200

    Configure bootstrap and dev backend values

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cnapp-tfstate"
-    storage_account_name = "sttfcnapp"
+    resource_group_name  = "rg-cnappuser6-tfstate"
+    storage_account_name = "sttfcnappuser6"
     container_name       = "tfstate-dev"
     key                  = "dev.terraform.tfstate"
     use_oidc             = true
